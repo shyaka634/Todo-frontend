276505875beabc64f6990474f3773d71c07fe05f
shyaka634 <tshyaka6@gmail.com>
2026-01-20 09:07:27 +0200

    Changed BE link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://todo-app-4-ooas.onrender.com/api",
   withCredentials: true
 });
 
